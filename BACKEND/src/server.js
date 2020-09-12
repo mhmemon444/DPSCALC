bcc09c824859c9de19170a244e50d38827e5a599
Hassan Memon <mhmemon@connect.ust.hk>
2020-09-12 22:52:16 +0500

    basic frontend folders added

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -1,13 +1,17 @@
+const path = require('path')
 const express = require('express');
 
 const server = express();
+const publicDirectoryPath = path.join(__dirname, '../../FRONTEND/public')
+
+server.use(express.static(publicDirectoryPath));
 
 server.get('', (req, res) => { // route :: 'app.com' -> ( --> direct to homepage <-- )
-    res.send('Hello express!')
+    res.sendFile( path.join(__dirname, '../../FRONTEND/public/pages/index.html') )
 })
 
 server.get('/help', (req, res) => { // (( ROUTING EXAMPLE )) route :: 'app.com/help' -> ( --> direct to help page <-- )
-    res.send('Help page')
+    res.sendFile( path.join(__dirname, '../../FRONTEND/public/pages/help.html') )
 })
 
 server.listen(3000, () => {
