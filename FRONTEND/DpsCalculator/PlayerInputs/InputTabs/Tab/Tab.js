450fb2ed75fa42858431f33d2a58eb1078ef73ae
Hassan Memon <mhmemon@connect.ust.hk>
2020-09-26 22:46:48 +0500

    input tabs added to PlayerInputs box

---
+++ FRONTEND/DpsCalculator/PlayerInputs/InputTabs/Tab/Tab.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import './Tab.css'
+
+const Tab = (props) => {
+
+    var imgSrc = null;
+    switch (props.type) {
+        case "Combat":
+            imgSrc = "https://oldschool.runescape.wiki/images/8/8f/Combat_icon.png?93d63";
+            break;
+        case "Skills":
+            imgSrc = "https://oldschool.runescape.wiki/images/a/a3/Skills_icon.png?a8e9f";
+            break;
+        case "Equipment":
+            imgSrc = "https://oldschool.runescape.wiki/images/5/50/Worn_equipment.png?124cf";
+            break;
+        case "Potions":
+            imgSrc = "https://oldschool.runescape.wiki/images/a/ae/Attack_potion%284%29.png?ab5d5";
+            break;
+        case "Prayers":
+            imgSrc = "https://oldschool.runescape.wiki/images/f/f2/Prayer_icon.png?ca0dc";
+            break;
+    }
+
+    return (
+        <div className="tab">
+            <img src={imgSrc}/>
+        </div>
+    )
+}
+
+export default Tab
--- FRONTEND/DpsCalculator/PlayerInputs/PlayerInputs.js
+++ FRONTEND/DpsCalculator/PlayerInputs/PlayerInputs.js
@@ -4,13 +4,15 @@ import './PlayerInputs.css'
 
 import InputTabs from './InputTabs/InputTabs';
 
-const PlayerInputs = (props) => {
-    return (
-        <div className="playerinputs">
-            <div style={{textAlign: 'center', marginTop: '5px'}}><span className="playerinputstext">Player Inputs</span></div>
-            <InputTabs />
-        </div>
-    )
+class PlayerInputs extends React.Component {
+    render() {
+        return (
+            <div className="playerinputs">
+                <div style={{ textAlign: 'center', marginTop: '5px' }}><span className="playerinputstext">Player Inputs</span></div>
+                <InputTabs />
+            </div>
+        )
+    }
 }
 
 export default PlayerInputs
