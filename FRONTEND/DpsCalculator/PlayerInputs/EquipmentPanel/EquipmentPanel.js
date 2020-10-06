0444d16ca443db8bcc5daa7d01bb7d3f40e6f8a5
Hassan Memon <mhmemon@connect.ust.hk>
2020-10-07 02:19:02 +0500

    gear kept on tab switch fixed

--- FRONTEND/DpsCalculator/PlayerInputs/EquipmentPanel/EquipmentPanel.js
+++ FRONTEND/DpsCalculator/PlayerInputs/EquipmentPanel/EquipmentPanel.js
@@ -8,18 +8,7 @@ import './EquipmentPanel.css';
 class EquipmentPanel extends React.Component {
     state = {
         showModal: false,
-        selectedSlot: null,
-        cape: null,
-        head: null,
-        neck: null,
-        ammo: null,
-        weapon: null,
-        body: null,
-        shield: null,
-        legs: null,
-        hands: null,
-        feet: null,
-        ring: null
+        selectedSlot: null
     }
 
     modalClickHandler = () => { //hide modal when modal is clicked
@@ -35,90 +24,24 @@ class EquipmentPanel extends React.Component {
         })
     }
 
-    setCape = (capeObj) => {
-        this.setState({cape: capeObj}, () => {
-            console.log(this.state)
-        })
-    }
-
-    setHead = (headObj) => {
-        this.setState({head: headObj}, () => {
-            console.log(this.state)
-        })
-    }
-
-    setNeck = (neckObj) => {
-        this.setState({neck: neckObj}, () => {
-            console.log(this.state)
-        })
-    }
-
-    setAmmo = (ammoObj) => {
-        this.setState({ammo: ammoObj}, () => {
-            console.log(this.state)
-        })
-    }
-
-    setWep = (wepObj) => {
-        this.setState({weapon: wepObj}, () => {
-            console.log(this.state)
-        })
-    }
-
-    setBody = (bodyObj) => {
-        this.setState({body: bodyObj}, () => {
-            console.log(this.state)
-        })
-    }
-
-    setShield = (shieldObj) => {
-        this.setState({shield: shieldObj}, () => {
-            console.log(this.state)
-        })
-    }
-
-    setHands = (handsObj) => {
-        this.setState({hands: handsObj}, () => {
-            console.log(this.state)
-        })
-    }
-
-    setFeet = (feetObj) => {
-        this.setState({feet: feetObj}, () => {
-            console.log(this.state)
-        })
-    }
-
-    setRing = (ringObj) => {
-        this.setState({ring: ringObj}, () => {
-            console.log(this.state)
-        })
-    }
-
-    setLegs = (legsObj) => {
-        this.setState({legs: legsObj}, () => {
-            console.log(this.state)
-        })
-    }
-
     render() {
         return (
             <div className="equipmentpanel">
-                {this.state.showModal ? <Modal legs={this.state.legs} setLegs={this.setLegs} ring={this.state.ring} setRing={this.setRing} feet={this.state.feet} setFeet={this.setFeet} hands={this.state.hands} setHands={this.setHands} shield={this.state.shield} setShield={this.setShield} body={this.state.body} setBody={this.setBody} wep={this.state.weapon} setWep={this.setWep} ammo={this.state.ammo} setAmmo={this.setAmmo} neck={this.state.neck} setNeck={this.setNeck} head={this.state.head} setHead={this.setHead} selectedSlot={this.state.selectedSlot} cape={this.state.cape} setCape={this.setCape} click={this.modalClickHandler}/> : null }
+                {this.state.showModal ? <Modal legs={this.props.legs} setLegs={this.props.setLegs} ring={this.props.ring} setRing={this.props.setRing} feet={this.props.feet} setFeet={this.props.setFeet} hands={this.props.hands} setHands={this.props.setHands} shield={this.props.shield} setShield={this.props.setShield} body={this.props.body} setBody={this.props.setBody} wep={this.props.wep} setWep={this.props.setWep} ammo={this.props.ammo} setAmmo={this.props.setAmmo} neck={this.props.neck} setNeck={this.props.setNeck} head={this.props.head} setHead={this.props.setHead} selectedSlot={this.state.selectedSlot} cape={this.props.cape} setCape={this.props.setCape} click={this.modalClickHandler}/> : null }
                 <div className="equipmentrow">
-                    <EquipmentIcon head={this.state.head} type="head_slot" click={this.iconClickHandler}/>
+                    <EquipmentIcon head={this.props.head} type="head_slot" click={this.iconClickHandler}/>
                 </div>
                 <div className="equipmentrow">
-                    <EquipmentIcon cape={this.state.cape} type="cape_slot" narrow={true} click={this.iconClickHandler}/> <EquipmentIcon neck={this.state.neck} type="neck_slot" narrow={true} click={this.iconClickHandler}/> <EquipmentIcon ammo={this.state.ammo} type="ammo_slot" narrow={true} click={this.iconClickHandler}/>
+                    <EquipmentIcon cape={this.props.cape} type="cape_slot" narrow={true} click={this.iconClickHandler}/> <EquipmentIcon neck={this.props.neck} type="neck_slot" narrow={true} click={this.iconClickHandler}/> <EquipmentIcon ammo={this.props.ammo} type="ammo_slot" narrow={true} click={this.iconClickHandler}/>
                 </div>
                 <div className="equipmentrow">
-                    <EquipmentIcon weapon={this.state.weapon} type="weapon_slot" click={this.iconClickHandler}/> <EquipmentIcon body={this.state.body} type="body_slot" click={this.iconClickHandler}/> <EquipmentIcon shield={this.state.shield} type="shield_slot" click={this.iconClickHandler}/>
+                    <EquipmentIcon weapon={this.props.wep} type="weapon_slot" click={this.iconClickHandler}/> <EquipmentIcon body={this.props.body} type="body_slot" click={this.iconClickHandler}/> <EquipmentIcon shield={this.props.shield} type="shield_slot" click={this.iconClickHandler}/>
                 </div>
                 <div className="equipmentrow">
-                    <EquipmentIcon legs={this.state.legs} type="legs_slot" click={this.iconClickHandler}/>
+                    <EquipmentIcon legs={this.props.legs} type="legs_slot" click={this.iconClickHandler}/>
                 </div>
                 <div className="equipmentrow">
-                    <EquipmentIcon hands={this.state.hands} type="hands_slot" click={this.iconClickHandler}/> <EquipmentIcon feet={this.state.feet} type="feet_slot" click={this.iconClickHandler}/> <EquipmentIcon ring={this.state.ring} type="ring_slot" click={this.iconClickHandler}/>
+                    <EquipmentIcon hands={this.props.hands} type="hands_slot" click={this.iconClickHandler}/> <EquipmentIcon feet={this.props.feet} type="feet_slot" click={this.iconClickHandler}/> <EquipmentIcon ring={this.props.ring} type="ring_slot" click={this.iconClickHandler}/>
                 </div>
 
                 
--- FRONTEND/DpsCalculator/PlayerInputs/PlayerInputs.js
+++ FRONTEND/DpsCalculator/PlayerInputs/PlayerInputs.js
@@ -26,7 +26,20 @@ class PlayerInputs extends React.Component {
         //Prayers:
         selectedPrayers: [
 
-        ]
+        ],
+
+        //Selected gear:
+        cape: null,
+        head: null,
+        neck: null,
+        ammo: null,
+        weapon: null,
+        body: null,
+        shield: null,
+        legs: null,
+        hands: null,
+        feet: null,
+        ring: null
     }
 
     selectTabHandler = (tab) => {
@@ -43,6 +56,7 @@ class PlayerInputs extends React.Component {
         })
     }
 
+    //Prayer tab
     prayerClickHandler = (prayerName, active) => {
         var selPrayers = [...(this.state.selectedPrayers)];
         if (active == true) {
@@ -56,6 +70,74 @@ class PlayerInputs extends React.Component {
         this.setState({
             selectedPrayers: [...selPrayers]
         }, () => {
+            console.log(this.state)
+        })
+    }
+
+
+    //Equipment tab
+    setCape = (capeObj) => {
+        this.setState({cape: capeObj}, () => {
+            console.log(this.state)
+        })
+    }
+
+    setHead = (headObj) => {
+        this.setState({head: headObj}, () => {
+            console.log(this.state)
+        })
+    }
+
+    setNeck = (neckObj) => {
+        this.setState({neck: neckObj}, () => {
+            console.log(this.state)
+        })
+    }
+
+    setAmmo = (ammoObj) => {
+        this.setState({ammo: ammoObj}, () => {
+            console.log(this.state)
+        })
+    }
+
+    setWep = (wepObj) => {
+        this.setState({weapon: wepObj}, () => {
+            console.log(this.state)
+        })
+    }
+
+    setBody = (bodyObj) => {
+        this.setState({body: bodyObj}, () => {
+            console.log(this.state)
+        })
+    }
+
+    setShield = (shieldObj) => {
+        this.setState({shield: shieldObj}, () => {
+            console.log(this.state)
+        })
+    }
+
+    setHands = (handsObj) => {
+        this.setState({hands: handsObj}, () => {
+            console.log(this.state)
+        })
+    }
+
+    setFeet = (feetObj) => {
+        this.setState({feet: feetObj}, () => {
+            console.log(this.state)
+        })
+    }
+
+    setRing = (ringObj) => {
+        this.setState({ring: ringObj}, () => {
+            console.log(this.state)
+        })
+    }
+
+    setLegs = (legsObj) => {
+        this.setState({legs: legsObj}, () => {
             console.log(this.state)
         })
     }
@@ -74,7 +156,7 @@ class PlayerInputs extends React.Component {
                     {tabs}
                 </div>
 
-                { this.state.Equipment ? <EquipmentPanel /> 
+                { this.state.Equipment ? <EquipmentPanel legs={this.state.legs} setLegs={this.setLegs} ring={this.state.ring} setRing={this.setRing} feet={this.state.feet} setFeet={this.setFeet} hands={this.state.hands} setHands={this.setHands} shield={this.state.shield} setShield={this.setShield} body={this.state.body} setBody={this.setBody} wep={this.state.weapon} setWep={this.setWep} ammo={this.state.ammo} setAmmo={this.setAmmo} neck={this.state.neck} setNeck={this.setNeck} head={this.state.head} setHead={this.setHead} selectedSlot={this.state.selectedSlot} cape={this.state.cape} setCape={this.setCape}/> 
                 : this.state.Prayers ? <PrayerPanel selectedPrayers={this.state.selectedPrayers} prayerClick={this.prayerClickHandler}/>
                 : null }
 
