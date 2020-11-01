4ef2e7cdeeacd466e2e46c969d98e824a11031b6
Hassan Memon <mhmemon@connect.ust.hk>
2020-11-01 06:03:39 +0500

    cb attributes panel initialization

---
+++ FRONTEND/DpsCalculator/CombatAttributes/CombatAttributes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './CombatAttributes.css';
+
+class CombatAttributes extends React.Component {
+    state = {
+
+    }
+    render() {
+        return (
+            <div className="combat-attributes">
+                COMBAT-ATTRIBS
+            </div>
+        )
+    }
+}
+
+export default CombatAttributes;
--- FRONTEND/DpsCalculator/DpsCalculator.js
+++ FRONTEND/DpsCalculator/DpsCalculator.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import PlayerInputs from './PlayerInputs/PlayerInputs'
+import PlayerInputs from './PlayerInputs/PlayerInputs';
+import CombatAttributes from './CombatAttributes/CombatAttributes';
+
+import './DpsCalculator.css';
 
 class DpsCalculator extends React.Component {
     render() {
         return (
-            <div style={{marginTop: "50px"}}>
+            <div className="calc-overall-layout" style={{marginTop: "50px"}}>
                 {/* Dps calc react code is mounted here */}
                 <PlayerInputs />
+                <CombatAttributes />
             </div>
         )
     }
