50cf209bb1503702ebfbc3fe45fa6cd3e2821fff
Hassan Memon <mhmemon@connect.ust.hk>
2021-01-03 19:15:06 +0200

    cb attributes sign added

--- FRONTEND/DpsCalculator/CombatAttributes/AttributeRow/IndividualAttribute/IndividualAttribute.js
+++ FRONTEND/DpsCalculator/CombatAttributes/AttributeRow/IndividualAttribute/IndividualAttribute.js
@@ -6,7 +6,7 @@ const IndividualAttribute = (props) => {
     return (
         <div className="indl-attr">
             <img style={{height: '20px'}} src={props.icon} />
-            <div className="attrib-value">{props.attrib}</div>
+            <div className="attrib-value">{props.attrib >= 0 ? '+'+props.attrib : props.attrib }</div>
         </div>
     )
 }
